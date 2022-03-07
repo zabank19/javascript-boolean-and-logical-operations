523ad1e4dad502c01db9a5c9baa75d391a19940e
Napat <napat.wongchr@gmail.com>
2022-03-07 15:40:38 +0700

    modify ex

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,9 @@
-let is18Up = true;
+let isAgeOver18 = true;
 let isThai = false;
 let hasCriminalBlacklist = true;
+
+let isAllow = null;
+// Start coding here
+
+
+console.log (isAllow); // Result should be false
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,10 @@
+let lastMonthPaidMoreThan4000 = true;
+let isPlatinum = true;
 let isSunday = true;
-let isPlatinumMember = false;
-let isFirstTime = true;
+let hasBoughtProductFromITCategory = true;
+let hasAttendedDiscountEvent = false;
+
+let hasPromotion = null;
+//Start coding here
+
+console.log(hasPromotion); // Result should be false
